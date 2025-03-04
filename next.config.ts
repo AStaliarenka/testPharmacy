d57422d6ff8e260ac1888998ad10a594335379fe
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-04 11:00:47 +0300

    feat: changed next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // TODO: check images config
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/215x215/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
