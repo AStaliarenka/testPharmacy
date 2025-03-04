4fcb4cd493010fc6485dfbdefec21f7b14fa9597
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-04 11:06:24 +0300

    feat: added about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+        <>
+            <a href="http://www.linkedin.com/in/aleksei-stoliarenko">
+                Aleksey Stoliarenko
+            </a>
+        </>
+    )
+}
+
+export default About
