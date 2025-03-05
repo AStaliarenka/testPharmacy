f2fafc4d8149ba35fa4190fc9b9c553aaca03be0
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-05 20:15:51 +0300

    feat: added types and constants

---
+++ src/react/views/pharmProducts/@types.ts
@@ -0,0 +1,33 @@
+import { ObjectValues } from "@/scripts/helpers/types"
+import { FILTERS_NAMES, PRODUCTS_FIELDS } from "./constants"
+
+type PharmProductPrice = number | ""
+type PharmProductIsByPrescription = boolean | ""
+
+export type ProductFilter = ObjectValues<typeof FILTERS_NAMES>
+
+export type SelectedFilters = {
+    [FILTERS_NAMES.price]?: {min: PharmProductPrice, max: PharmProductPrice},
+    [FILTERS_NAMES.isByPrescription]?: boolean,
+    [FILTERS_NAMES.country]?: string[],
+    [FILTERS_NAMES.brand]?: string[],
+}
+
+/* TODO: there are cases, when some filter fields (ex. country) don`t have any value in ALL products
+in that case we dont need to show some filter (ex. country) */
+export type AllFiltersValues = {
+    [FILTERS_NAMES.price]: {min: PharmProductPrice, max: PharmProductPrice},
+    // [FILTERS_NAMES.isByPrescription]: boolean,
+    [FILTERS_NAMES.country]: string[] | [""],
+    [FILTERS_NAMES.brand]: string[] | [""],
+}
+
+export type TransformedPharmProductsData = {
+    [PRODUCTS_FIELDS.id]: number,
+    [PRODUCTS_FIELDS.title]: string,
+    [PRODUCTS_FIELDS.price]: PharmProductPrice,
+    [PRODUCTS_FIELDS.image]: string
+    [PRODUCTS_FIELDS.isByPrescription]: PharmProductIsByPrescription,
+    [PRODUCTS_FIELDS.country]: string,
+    [PRODUCTS_FIELDS.brand]: string,
+}
---
+++ src/react/views/pharmProducts/constants.ts
@@ -0,0 +1,16 @@
+export const PRODUCTS_FIELDS = {
+    title: "title",
+    id: "id",
+    price: "price",
+    country: "country",
+    brand: "brand",
+    isByPrescription: "isByPrescription",
+    image: "image",
+} as const
+
+export const FILTERS_NAMES = {
+    price: PRODUCTS_FIELDS.price,
+    country: PRODUCTS_FIELDS.country,
+    brand: PRODUCTS_FIELDS.brand,
+    isByPrescription: PRODUCTS_FIELDS.isByPrescription,
+} as const
